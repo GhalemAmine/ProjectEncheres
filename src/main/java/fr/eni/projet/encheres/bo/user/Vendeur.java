48a0f43d1d65f1bcea9a017636e717a84f8be002
Will <freyer.will@gmail.com>
2021-12-07 16:10:26 +0100

    Ajout des classes du package user et de la classe Adresse

---
+++ src/main/java/fr/eni/projet/encheres/bo/user/Vendeur.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package fr.eni.projet.encheres.bo.user;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.eni.projet.encheres.bo.Adresse;
+
+/**
+ * @author William
+ *
+ */
+public class Vendeur extends Utilisateur {
+
+	private int credit = 0;
+
+	// TODO Remplacer String par Article
+	private List<String> listeArticles = new ArrayList<String>();
+
+	public Vendeur() {
+
+	}
+
+	public Vendeur(String nom, String prenom, String pseudo, String email, Adresse adresse) {
+		super(nom, prenom, pseudo, email, adresse);
+	}
+
+	public Vendeur(Integer id, String nom, String prenom, String pseudo, String email, Adresse adresse) {
+		super(id, nom, prenom, pseudo, email, adresse);
+	}
+
+	public int getCredit() {
+		return this.credit;
+	}
+
+	public void setCredit(int credit) {
+		this.credit = credit;
+	}
+
+	public List<String> getListeArticles() {
+		return this.listeArticles;
+	}
+
+	public void setListeArticles(List<String> listeArticles) {
+		this.listeArticles = listeArticles;
+	}
+
+	public void ajouterArticle(String article) {
+		this.listeArticles.add(article);
+		// ne pas oublier de set l'utilisateur sur l'article à ce moment aussi
+	}
+
+	@Override
+	protected void setAdministrateur(boolean administrateur) {
+		System.err.println("tu pensais vraiment qu'avec tes droits de base tu pourrais devenir un Admin ?");
+	}
+
+}
