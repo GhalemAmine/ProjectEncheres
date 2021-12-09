76ff361376b3d7c4c16ba3e67dc18630a7b2fdf8
Tarda <Tarda@LAPTOP-D458IA8S>
2021-12-09 10:11:14 +0100

    MAJ Catégorie DAL

--- src/main/java/fr/eni/projet/encheres/dal/jdbc/CategorieJdbcImpl.java
+++ src/main/java/fr/eni/projet/encheres/dal/jdbc/CategorieJdbcImpl.java
@@ -6,13 +6,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import fr.eni.projet.encheres.bo.Categorie;
-import fr.eni.projet.encheres.bo.ExempleObjet;
 import fr.eni.projet.encheres.dal.ConnectionProvider;
 import fr.eni.projet.encheres.dal.DALException;
 import fr.eni.projet.encheres.dal.DAOCategorie;
-import fr.eni.projet.encheres.dal.DAOExempleObjet;
-import fr.eni.projet.encheres.dal.jdbc.DAOJdbcImpl;
-import fr.eni.projet.encheres.dal.jdbc.JdbcTools;
+
 
 /**
  * @author Alexandre Mchich
@@ -21,7 +18,7 @@ import fr.eni.projet.encheres.dal.jdbc.JdbcTools;
 public class CategorieJdbcImpl extends DAOJdbcImpl<Categorie> implements DAOCategorie {
 
 	String sqlDeleteByID = "delete from Categorie where id=?";
-	String sqlInsert = "insert into Categorie(XXXXX, XXXXXXX) values (?,?)";
+	String sqlInsert = "insert into Categorie(id, nom) values (?,?)";
 	String sqlSelectByID = "select id, nom from Categorie where id=?";
 	String sqlSelectAll = "select id, nom from Categorie";
 	String sqlUpdate = "update Categorie set id=?, nom=?, where id=? ";
@@ -40,18 +37,18 @@ public class CategorieJdbcImpl extends DAOJdbcImpl<Categorie> implements DAOCate
 
 			int index = 1;
 		
-			stmt.setString(index++, c.getnom());
-			if(c.getid() != null) {
-				stmt.setInt(index++, c.getid());
+			stmt.setString(index++, c.getNom());
+			if(c.getId() != null) {
+				stmt.setInt(index++, c.getId());
 			}
 		}
 
 	@Override
 	public Categorie createFromRS(ResultSet rs) throws SQLException {
 		
-		Categorie categorie;
-		categorie.setid(rs.getInt("id"));
-		categorie.setnom(rs.getString("nom"));
+		Categorie categorie = new Categorie();
+		categorie.setId(rs.getInt("id"));
+		categorie.setNom(rs.getString("nom"));
 		
 //			al.setNom(rs.getString(2));
 //			al.setIdRepas(3);
@@ -60,7 +57,7 @@ public class CategorieJdbcImpl extends DAOJdbcImpl<Categorie> implements DAOCate
 
 	@Override
 	public void delete(Categorie categorie) throws DALException {
-		this.deleteByID(categorie.getid());
+		this.deleteByID(categorie.getId());
 
 	}
 
@@ -72,7 +69,7 @@ public class CategorieJdbcImpl extends DAOJdbcImpl<Categorie> implements DAOCate
 				ResultSet rs = null;) {
 			completeStmt(categorie, stmt);
 			stmt.execute();
-			categorie.setid(JdbcTools.recupID(stmt, rs));
+			categorie.setId(JdbcTools.recupID(stmt, rs));
 		} catch (SQLException e) {
 			throw new DALException("erreur de requete Insert", e);
 
