a0e7923b4038a95e4307dc73f88407169c6a4eff
Will <freyer.will@gmail.com>
2021-12-13 13:02:30 +0100

    Transformation SelectAll sur Encheres pour limiter le couplage

--- src/main/java/fr/eni/projet/encheres/dal/jdbc/EnchereJdbcImpl.java
+++ src/main/java/fr/eni/projet/encheres/dal/jdbc/EnchereJdbcImpl.java
@@ -11,10 +11,9 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import fr.eni.projet.encheres.bll.ArticleManager;
-import fr.eni.projet.encheres.bll.BLLException;
-import fr.eni.projet.encheres.bll.UtilisateurManager;
+import fr.eni.projet.encheres.bo.Adresse;
 import fr.eni.projet.encheres.bo.Article;
+import fr.eni.projet.encheres.bo.Categorie;
 import fr.eni.projet.encheres.bo.Enchere;
 import fr.eni.projet.encheres.bo.user.Vendeur;
 import fr.eni.projet.encheres.dal.ConnectionProvider;
@@ -31,7 +30,7 @@ public class EnchereJdbcImpl extends DAOJdbcImpl<Enchere> implements DAOEnchere 
 	String sqlDeleteByID = "delete from Encheres where idUtilisateur=? AND idArticle=?";
 	String sqlInsert = "insert into Encheres(idUtilisateur, idArticle, dateEnchere, montantEnchere) values (?,?,?,?)";
 	String sqlSelectByID = "select idUtilisateur, idArticle, dateEnchere, montantEnchere from ENCHERES where idUtilisateur=? AND idArticle=?";
-	String sqlSelectAll = "select idUtilisateur, idArticle, dateEnchere, montantEnchere from ENCHERES";
+	String sqlSelectAll = "select dateEnchere, montantEnchere, Articles.id, nomArticle, description, dateDebutEncheres, dateFinEncheres, idCategorie,Utilisateurs.id, pseudo, nom, prenom, email, telephone, rue, codePostal, ville, motDePasse, credit, administrateur from ENCHERES inner join ARTICLES on idArticle=Articles.id inner join UTILISATEURS on ENCHERES.idUtilisateur=UTILISATEURS.id";
 	String sqlUpdate = "update ENCHERES set idUtilisateur=?, dateEnchere=?, montantEnchere=?, where idUtilisateur=? AND idArticle=?";
 	String sqlTruncate = "truncate table ENCHERES";
 	String sqlSelectByArticle = "select idUtilisateur, idArticle, dateEnchere, montantEnchere from ENCHERES where idArticle=?";
@@ -182,27 +181,47 @@ public class EnchereJdbcImpl extends DAOJdbcImpl<Enchere> implements DAOEnchere 
 	@Override
 	public Enchere createFromRS(ResultSet rs) throws SQLException {
 
+		//génération de l'adresse
+		
+		Adresse adr = new Adresse();
+		
+		adr.setRue(rs.getString("rue"));
+		adr.setCodePostal(rs.getString("codePostal"));
+		adr.setVille("ville");
+		
+		
+	// génération du vendeur	
+		Vendeur vendeur = new Vendeur();
+		
+		vendeur.setId(rs.getInt("idUtilisateur"));
+		vendeur.setPseudo(rs.getString("pseudo"));
+		vendeur.setNom(rs.getString("nom"));
+		vendeur.setPrenom(rs.getString("prenom"));
+		vendeur.setEmail(rs.getString("email"));
+		vendeur.setTelephone(rs.getString("telephone"));
+		vendeur.setAdresse(adr);
+		vendeur.setMdp(rs.getString("motDePasse"));
+		vendeur.setCredit(rs.getInt("credit"));
+		vendeur.setAdministrateur(rs.getBoolean("administreur"));
+//génération de la Categorie
+		
+		Categorie cat = new Categorie();
+		cat.setId(rs.getInt("idCategorie"));
+		cat.setNom("libelle");
+//génération de l'article
+		Article art = new Article();
+
+		
+		art.setId(rs.getInt("idArticle"));
+		art.setNomArticle(rs.getString("nomArticle"));
+		art.setDescription(rs.getString("description"));
+		art.setDateDebut(rs.getDate("dateDebutEncheres"));
+		art.setDateFin(rs.getDate("dateFinEncheres"));
+		art.setCategorie(cat);
+		art.setUtilisateur(vendeur);
+
+//génération de l'enchere : 
 		Enchere enc = new Enchere();
-		Article art;
-		Vendeur ven;
-		Integer idArticle = rs.getInt("idArticle");
-		Integer idUser = rs.getInt("idUtilisateur");
-
-		try {
-			ArticleManager artMan = new ArticleManager();
-			art = artMan.getItem(idArticle);
-			enc.setArticle(art);
-		} catch (BLLException e) {
-			e.printStackTrace();
-		}
-
-		try {
-			UtilisateurManager userMan = new UtilisateurManager();
-			ven = (Vendeur) userMan.getItem(idUser);
-			enc.setVendeur(ven);
-		} catch (BLLException e) {
-			e.printStackTrace();
-		}
 
 		enc.setDateEnchere(rs.getDate("dateEnchere"));
 		enc.setMontantEnchere(rs.getInt("montantEnchere"));
