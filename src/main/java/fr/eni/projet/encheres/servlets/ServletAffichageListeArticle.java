fcde629755d05eb9d1aa91faf78d6997e654c77b
Spague / Greg <gregory.bonnefond@yahoo.fr>
2021-12-14 17:07:51 +0100

    Affichage liste article pour jspf

--- src/main/java/fr/eni/projet/encheres/servlets/ServletAffichageListeArticle.java
+++ src/main/java/fr/eni/projet/encheres/servlets/ServletAffichageListeArticle.java
@@ -59,10 +59,6 @@ public class ServletAffichageListeArticle extends HttpServlet {
 
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		doGet(request, response);
